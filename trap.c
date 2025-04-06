5effda8a115717dbb1a8a99e019dcbef15981c75
Ethan Kocses <89274912+EthanK888@users.noreply.github.com>
2025-04-06 19:35:16 -0400

    Cleaning up code and adding comments

--- trap.c
+++ trap.c
@@ -111,20 +111,28 @@ trap(struct trapframe *tf)
     
     lapiceoi();
     break;
+    
+  //Project 3: check for page faults
   case T_PGFLT:{
+    //Track the number of page faults each program has
     myproc()->pagefaults++;
     uint a = rcr2();
     cprintf("\nPAGE FAULT %d FOR PROCESS %d\n", myproc()->pagefaults, myproc()->pid);
     cprintf("memory address causing the page fault: 0x%x\n", a);
+    
+    //Round the faulting address down to a page boundary
     a = PGROUNDDOWN(a);
     cprintf("START OF PAGE FOR THIS MEMORY ACCESS: 0x%x\n", a);
   
+    //Lazy allocator
     #ifdef LAZY
+      //Allocate a frame for the process
       char * mem = kalloc();
       if(mem == 0){
         cprintf("out of memory\n");
       } else {
         memset(mem, 0, PGSIZE);
+        //Map the frame to the page table for the process
         if (mappages(myproc()->pgdir, (char *)a, PGSIZE, V2P(mem), PTE_W | PTE_U) > -1)
         {
           cprintf("page table entry added to cover all virtual addresses from 0x%x to 0x%x\n", a, a+PGSIZE-1);
@@ -133,11 +141,15 @@ trap(struct trapframe *tf)
         cprintf("error mapping pages\n");
         kfree(mem);
       }
+
+    //Locality based allocator
     #elif defined(LOCALITY)
       int error = 0;
+      //Track which pages have been allocated in case of an error
       uint allocatedPages[5] = {0};
       int numPagesAllocated = 0;
 
+      //Allocate 5 pages - the one containing the faulting address and the next 4
       for(int i = 0; i < 5; i++, a += PGSIZE){ 
         char * mem = kalloc();
         if(mem == 0){
@@ -161,6 +173,7 @@ trap(struct trapframe *tf)
       }
 
       if(error){
+        //If there's any error, deallocate each page given to the process before the error and kill the process
         for(int i = 0; numPagesAllocated; i++){
           deallocuvm(myproc()->pgdir, allocatedPages[i] + PGSIZE, allocatedPages[i]);
         }
