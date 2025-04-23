bd656e5c12f7720c08a4508e84392661d6d30e55
Ethan Kocses <ethan_k888@outlook.com>
2025-04-22 21:31:10 -0400

    add symlink_test

---
+++ symlink_test.c
@@ -0,0 +1,14 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int main(){
+    symlink("txtfile", "symlink1");
+    //int symlink1 = open("symlink1", O_RDONLY);
+
+    
+
+
+    exit();
+}
