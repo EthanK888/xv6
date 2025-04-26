24c189d5e0de3fdf47054132c66982d7a0706801
Ethan Kocses <89274912+EthanK888@users.noreply.github.com>
2025-04-25 21:46:10 -0400

    Working on symlink_test

--- symlink_test.c
+++ symlink_test.c
@@ -4,12 +4,40 @@
 #include "fcntl.h"
 
 int main(){
+    //Create a symlink to txtfile
     symlink("txtfile", "symlink1");
     int symlink1 = open("symlink1", O_RDONLY);
-    printf(1, "fd: %d\n", symlink1);
-    //char* buffer = malloc(10 * sizeof(char));
-    //read(symlink1, buffer, 10);
+    //printf(1, "fd: %d\n", symlink1);
+    
+    //Read from the symlink
+    char* buffer = malloc(10 * sizeof(char));
+    read(symlink1, buffer, 10);
+    printf(1, "Read from symlink: %s\n", buffer);
 
+    close(symlink1);
+
+    //Create a loop of symlinks (should fail to open)
+    symlink("symlinkLoop1", "symlinkLoop2");
+    symlink("symlinkLoop2", "symlinkLoop1");
+
+    //Read from the symlink using O_NOFOLLOW
+    char* buffer2 = malloc(7 * sizeof(char));
+    symlink1 = open("symlink1", O_NOFOLLOW);
+    read(symlink1, buffer2, 7);
+    printf(1, "Read from symlink using O_NOFOLLOW: %s\n", buffer2);
+
+    //Testing that link and unlink don't follow symlinks
+    link("symlink1", "link");
+    int link = open("link", O_RDONLY);
+    read(link, buffer2, 7);
+    printf(1, "Reading from link. If it reads the path that symlink1 holds, then it didn't follow the symlink.\n%s\n", buffer2);
+    unlink("link");
+
+    close(symlink1);
+    close(link);
+
+    free(buffer);
+    free(buffer2);
 
     exit();
 }
